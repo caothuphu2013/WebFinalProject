5a6006ced3d42ffac4c1f7337e4aada3e2b17e3c
Thu Nguyen <nguyenhotoanthuap97@gmail.com>
2018-06-25 03:24:36 +0700

    admin manage product

--- config/router.js
+++ config/router.js
@@ -1,6 +1,6 @@
 const index = require("../apps/controllers/index");
-const admin = require("../apps/controllers/admin.js")
-const errorPage = require("../apps/controllers/errorController.js")
+const admin = require("../apps/controllers/admin")
+const errorPage = require("../apps/controllers/errorController")
 
 module.exports = function(app) {
     //home
@@ -10,14 +10,20 @@ module.exports = function(app) {
     app.get("/shop/choose", index.products.searchProductsAuto);
     app.post("/shop/search", index.products.search);
 
-
     //single-product
     app.get("/product",index.singleproduct.getProduct);
 
     //admin
-    app.get("/dashboard", admin.loadDashboard);
-    app.get("/dashboardtype", admin.loadDashboardType);
-    app.get("/dashboardbrand", admin.loadDashboardBrand);
+    app.get("/dashboard", admin.dashboard.loadDashboard);
+    app.get("/dashboardtype", admin.dashboard.loadDashboardType);
+    app.get("/dashboardbrand", admin.dashboard.loadDashboardBrand);
+
+    //admin management
+    app.get("/management", admin.manage.loadProductManagement);
+    app.get("/management?manage=type", admin.manage.loadProductManagement);
+    app.get("/management?manage=product", admin.manage.loadProductManagement);
+    app.get("/management?manage=brand", admin.manage.loadProductManagement);
+    app.get("/management?manage=orders", admin.manage.loadProductManagement);
 
     //user
     app.get("/register", index.users.registerPage);
@@ -31,6 +37,6 @@ module.exports = function(app) {
     app.get("/profile/update", index.profile.updatePage);
     app.post('/profile/update',index.profile.updateInfo);
 
-        //Err0r
+    //Err0r
     app.use(errorPage);
 }
