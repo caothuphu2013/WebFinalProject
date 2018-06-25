eaee07b69c114fed4ae14dae3da7186db19b5983
billhcmus <billgates15897@gmail.com>
2018-06-26 00:39:06 +0700

    update homepage, csdl

--- apps/controllers/homeController.js
+++ apps/controllers/homeController.js
@@ -1,14 +1,25 @@
-const layout = require('../models/layout');
+const home = require('../models/home');
+const l = require('../models/layout');
 const q = require('q');
 
 let homeController = {
     loadHomePage : function(req, res) {
-        let p1 = layout.getBrands().catch(err=>{
+        let p1 = home.getLastestProduct().catch(err=>{
             console.log("Error: " + err);
         });
-        q.all([p1]).spread(function(){
+        let p2 = l.getBrands().catch(err=>{
+            console.log("Error: " + err);
+        });
+        let p3 = home.getBestSellingProduct().catch(err=>{
+            console.log("Error: " + err);
+        });
+
+        q.all([p1, p2, p3]).spread(function(temp1, temp2, temp3){
             res.render('_home/home', {
                 user: req.session.user,
+                ListProducts: temp1,
+                ListBrands: temp2,
+                ListBestSelling: temp3,
                 layout: "index"
             });
         });
--- apps/models/home.js
+++ apps/models/home.js
@@ -1,2 +1,27 @@
 //Xu ly database
 const db = require("./database.js")
+const q = require("q");
+
+exports.getLastestProduct = () => {
+    let d = q.defer();
+    let sql = "select * from product where product.ngaydang > '2017-5-5'";
+    db.query(sql, (error, results) => {
+        if (error) {
+            d.reject(error);
+        }
+        d.resolve(results);
+    });
+    return d.promise;
+}
+
+exports.getBestSellingProduct = () => {
+    let d = q.defer();
+    let sql = "select * from product where product.buyTimes > 5";
+    db.query(sql, (error, results) => {
+        if (error) {
+            d.reject(error);
+        }
+        d.resolve(results);
+    });
+    return d.promise;
+}
