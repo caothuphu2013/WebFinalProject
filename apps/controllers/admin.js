3a775104185be9b3301a9099544e1fd72d45172c
Thu Nguyen <nguyenhotoanthuap97@gmail.com>
2018-06-29 01:24:56 +0700

    add admin update product

--- apps/controllers/admin.js
+++ apps/controllers/admin.js
@@ -1,7 +1,9 @@
 const dashboardController = require("./dashboardController");
 const manageController = require("./manageController");
+const editProductsController = require("./editProductsController");
 
 module.exports = {
     dashboard: dashboardController,
     manage: manageController,
+    editProducts: editProductsController,
 }
