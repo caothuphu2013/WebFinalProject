d78c8f1722e5821310fafb764e043fb5a037a285
TienLaptop\Tien <tienandehit@gmail.com>
2018-06-29 12:11:54 +0700

    login admin

--- apps/controllers/manageController.js
+++ apps/controllers/manageController.js
@@ -3,18 +3,33 @@ const q = require('q');
 
 let manageController = {
     loadProductManagement : function(req, res) {
-        let p = categoryDB.getProducts().catch(err => {
+        if (!req.session.user)
+        {
+            req.session.prePage = req.originalUrl;
+            res.redirect('/login');
+        }
+        else if (req.session.user.type == 1) {
+            let p = categoryDB.getProducts().catch(err => {
                 console.log("Error: " + err)});
-        q.all([p]).spread(function(temp1) {
-            res.render("_admin/manage", {
-                products: temp1,
-                isProduct: true,
-                manageSession: 'admin_Manage_Product',
-                layout: "admin"
-            });
-        })
+            q.all([p]).spread(function(temp1) {
+                res.render("_admin/manage", {
+                    products: temp1,
+                    isProduct: true,
+                    manageSession: 'admin_Manage_Product',
+                    layout: "admin"
+                });
+            })
+        }
+        else
+        res.redirect('/error');
     },
     loadTypeManagement : function(req, res) {
+        if (!req.session.user)
+        {
+            req.session.prePage = req.originalUrl;
+            res.redirect('/login');
+        }
+        else if (req.session.user.type == 1) {
         let p = categoryDB.getType().catch(err => {
                 console.log("Error: " + err);});
         q.all([p]).spread(function(temp1) {
@@ -25,8 +40,17 @@ let manageController = {
                 layout: "admin"
             });
         })
+        }
+        else
+        res.redirect('/error');
     },
     loadBrandManagement : function(req, res) {
+        if (!req.session.user)
+        {
+            req.session.prePage = req.originalUrl;
+            res.redirect('/login');
+        }
+        else if (req.session.user.type == 1) {
         let p = categoryDB.getBrand().catch(err => {
                 console.log("Error: " + err);});
         q.all([p]).spread(function(temp1) {
@@ -38,8 +62,17 @@ let manageController = {
             
             });
         })
+        }
+        else
+        res.redirect('/error');
     },
     loadOrdersManagement : function(req, res) {
+        if (!req.session.user)
+        {
+            req.session.prePage = req.originalUrl;
+            res.redirect('/login');
+        }
+        else if (req.session.user.type == 1) {
         let p = categoryDB.getOrders().catch(err => {
                 console.log("Error: " + err);});
         let temp = [1];
@@ -53,14 +86,27 @@ let manageController = {
                 isempty: temp1.length === 0,
             });
         })
+        }
+        else
+        res.redirect('/error');
     },
     addProduct : function(req, res) {
+        if (!req.session.user)
+        {
+            req.session.prePage = req.originalUrl;
+            res.redirect('/login');
+        }
+        else if (req.session.user.type == 1) {
             res.render("_admin/manage", {
                 manageSession: 'admin_Add_Product',
                 layout: "admin",
                 isProduct: false,
                 true: 1 == 1
             });
+        }
+        else
+        res.redirect('/error');
+
     },
     insertProductToDB : function(req, res) {
         let productName = req.body.productName;
